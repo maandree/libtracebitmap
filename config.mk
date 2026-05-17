53a8559d99cffb1f65e849cbe5737a7727ea05cc
Mattias Andrée <m@maandree.se>
2026-05-17 18:49:43 +0200

    Checked with sanitizer
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- config.mk
+++ config.mk
@@ -3,6 +3,12 @@ MANPREFIX = $(PREFIX)/share/man
 
 CC = c99
 
+COMMON_SANITIZE = -fsanitize=alignment,shift,signed-integer-overflow,object-size,null,undefined,bounds,address
+CLANG_SANITIZE  = -O1 $(COMMON_SANITIZE),cfi -flto -fvisibility=hidden -fno-sanitize-trap=cfi
+GCC_SANITIZE    = -O1 $(COMMON_SANITIZE)
+#SANITIZE        = $(CLANG_SANITIZE)
+#SANITIZE        = $(GCC_SANITIZE)
+
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -Wall -O2
-LDFLAGS  = -s
+CFLAGS   = $(SANITIZE) -Wall -O2
+LDFLAGS  = $(SANITIZE) -s
